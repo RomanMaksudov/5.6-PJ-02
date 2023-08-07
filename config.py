16bad70363ddcb09f12e2f62a878976e17c948ab
Roman <129749091+RomanMaksudov@users.noreply.github.com>
2023-08-07 16:51:18 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = "6084994452:AAFPBkzf5AvC1EswQOZJtoaVIiOBQTfBdz4"
+
+keys = {
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+    'дирхам': 'AED',
+    'доги': 'DOGE',
+    'биткоин': 'BTC',
+}
